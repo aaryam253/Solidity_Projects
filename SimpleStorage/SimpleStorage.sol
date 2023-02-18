d382cff413cd4978a7bb3de8bf89808a3dc21046
aaryamNUS <aaryamps1@gmail.com>
2023-02-18 18:43:10 +0800

    Formatting issues

--- SimpleStorage/SimpleStorage.sol
+++ SimpleStorage/SimpleStorage.sol
@@ -1,21 +1,18 @@
-
-
-pragma solidity >= 0.6.0 <0.9.0;
+pragma solidity >=0.6.0 <0.9.0;
 
 contract SimpleStorage {
-
     // This will get initialised to the null value (i.e. 0 for unsigned numbers)
-    // Default access specifier is internal 
+    // Default access specifier is internal
     uint256 num;
 
     struct People {
         uint256 favNum;
         string name;
     }
-    
+
     People[] public people;
     mapping(string => uint256) public nameToFavNum;
-    
+
     // Memory and storage
     // Memory - data only stored during execution of the function
     // Storage - data persisted after function is executed
@@ -29,12 +26,12 @@ contract SimpleStorage {
     }
 
     // View and pure are non changing functions
-    function retrieve() public view returns(uint256) {
+    function retrieve() public view returns (uint256) {
         return num;
     }
 
     // Pure functions purely perform mathematical operations
-    function multiply(uint256 favNum) public pure returns(uint256) {
+    function multiply(uint256 favNum) public pure returns (uint256) {
         return favNum * 2;
     }
 }
